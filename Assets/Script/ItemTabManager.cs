d307158f5a9147abd68fde931b2cf6de646590ba
Kimyongchoen <dydcjs87@naver.com>
2021-05-16 07:18:43 +0900

    26

---
+++ Assets/Script/ItemTabManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemTabManager : MonoBehaviour
+{
+    public Text WeaponName;
+    public Text WeaponStats;
+    public Image WeaponImage;
+    
+    public Text ArmorName;
+    public Text ArmorStats;
+    public Image ArmorImage;
+    
+    public Text HatName;
+    public Text HatStats;
+    public Image HatImage;
+    
+    public Text GlovesName;
+    public Text GlovesStats;
+    public Image GlovesImage;
+    
+    public Text BootsName;
+    public Text BootsStats;
+    public Image BootsImage;
+    
+    public Text CloakName;
+    public Text CloakStats;
+    public Image CloakImage;
+
+    public PlayerStats playerStats_temp;
+    public ItemStats ItemStats;
+    private PlayerStats.Stats playerStats;
+
+    private void Awake()
+    {
+        playerStats = playerStats_temp.stats[0];
+        ChangeItemInfomation();
+    }
+
+    public void ChangeItemInfomation()
+    {
+        WeaponName.text = "+0 무기";
+        WeaponStats.text = "공격력 + 0";
+
+        if (playerStats.weapon >= 0)//무기 강화에 따른 데미지 UP 무기가 없으면 -1
+        {
+            WeaponName.text = "+" + playerStats.weapon + " 무기";
+            WeaponStats.text = "공격력 + " + ItemStats.weapon[playerStats.weapon];
+        }
+
+        ArmorName.text = "+0 갑옷";
+        ArmorStats.text = "방어력 +0";
+
+        if (playerStats.Armor>= 0)//갑옷 강화에 따른 방어력 UP 갑옷이 없으면 -1
+        {
+            ArmorName.text = "+" + playerStats.Armor + " 갑옷";
+            ArmorStats.text = "방어력 + " + ItemStats.Armor[playerStats.Armor];
+        }
+
+        HatName.text = "+0 투구";
+        HatStats.text = "체력 +0";
+
+        if (playerStats.hat >= 0)//투구 강화에 따른 체력 UP 투구가 없으면 -1
+        {
+            HatName.text = "+" + playerStats.hat + " 투구";
+            HatStats.text = "체력 + " + ItemStats.hat[playerStats.hat];
+        }
+
+        GlovesName.text = "+0 장갑";
+        GlovesStats.text = "치명타 확율 +0";
+
+        if (playerStats.Gloves >= 0)//장갑 강화에 따른 치명타 확율 UP 장갑이 없으면 -1
+        {
+            GlovesName.text = "+" + playerStats.Gloves + " 장갑";
+            GlovesStats.text = "치명타 확율 + " + ItemStats.Gloves[playerStats.Gloves]+"%";
+        }
+
+        BootsName.text = "+0 신발";
+        BootsStats.text = "회피력 +0";
+
+        if (playerStats.Boots >= 0)//신발 강화에 따른 회피력 UP 신발이 없으면 -1
+        {
+            BootsName.text = "+" + playerStats.Boots + " 신발";
+            BootsStats.text = "회피력 + " + ItemStats.Boots[playerStats.Boots];
+        }
+
+        CloakName.text = "+0 방패";
+        CloakStats.text = "체력 흡혈 +0";
+
+        if (playerStats.Cloak >= 0)//장갑 강화에 따른 치명타 확율 UP 장갑이 없으면 -1
+        {
+            CloakName.text = "+" + playerStats.Cloak + " 방패";
+            CloakStats.text = "체력흡혈 + " + ItemStats.Cloak[playerStats.Cloak]*100+"%";
+        }
+    }
+}
+
--- Assets/Script/PlayerTabManager.cs
+++ Assets/Script/PlayerTabManager.cs
@@ -23,10 +23,14 @@ public class PlayerTabManager : MonoBehaviour
     public Text goldBonus;
     public Text experience;
     public Text gold;
+    public Text ScreenGold;
+    public Text ScreenLevel;
+
 
     private int hp;
 
     public PlayerStats playerStats_temp;
+    public ItemStats ItemStats;
     private PlayerStats.Stats playerStats;
 
     private Sprite sprite_temp;
@@ -52,22 +56,69 @@ public class PlayerTabManager : MonoBehaviour
     }
     public void ChangeMonsterInfomation(int PlayerNum)
     {
-        Playername.text = "플레이어 이름 : " + playerStats.Playername; // 레벨
+        int AttackDamagePlus = 0;
+        int defensePlus = 0;
+
+        Playername.text = "플레이어 이름 : " + playerStats.Playername; // 플레이어 이름
+        
         level.text = "level : " + playerStats.level; // 레벨
+
+        ScreenLevel.text = "level "+ playerStats.level;//화면 상단 레벨 표시
+
         attackDamage.text = "공력력 : " + playerStats.attackDamageMin.ToString() + " - " + playerStats.attackDamageMax.ToString(); //공격력
-        health.text = "체력 : " + hp.ToString() + " / " + playerStats.health.ToString();//체력
+        if (ItemStats.AttackDamageUp > 0)//공격력 버프 증가시
+            AttackDamagePlus += ItemStats.AttackDamageUp;
+        if (playerStats.weapon >= 0)//무기 강화에 따른 데미지 UP 무기가 없으면 -1
+            AttackDamagePlus += ItemStats.weapon[playerStats.weapon];
+        if (AttackDamagePlus > 0)
+            attackDamage.text += " + (" + AttackDamagePlus + ")";
+
+        if (playerStats.hat >= 0)//투구 강화에 따른 체력 UP 투구가 없으면 -1
+        {
+            health.text = "체력  : " + hp.ToString() + " / " + (playerStats.health - ItemStats.hat[playerStats.hat]).ToString() + "+(" + ItemStats.hat[playerStats.hat] + ")";//체력
+        }
+        else
+        {
+            health.text = "체력  : " + hp.ToString() + " / " + playerStats.health.ToString();//체력
+        }
+
         defense.text = "방어력 : " + playerStats.defense.ToString();//방어력
+        if (ItemStats.DefenseUp > 0)//방어력 버프 증가시
+            defensePlus += ItemStats.DefenseUp;
+        if (playerStats.Armor >= 0)//갑옷 강화에 따른 방어력 UP 갑옷이 없으면 -1
+            defensePlus += ItemStats.Armor[playerStats.Armor];
+        if (defensePlus > 0)
+            defense.text += " + (" + defensePlus + ")";
+
         attackSpeed.text = "공격속도 : " + playerStats.attackSpeed.ToString(); //공격속도
+        
         attackRange.text = "공격범위 : " + playerStats.attackRange.ToString(); //공격범위
+        
         hit.text = "명중치 : " + playerStats.hit.ToString();//명중치
+        
         evasion.text = "회피치 : " + playerStats.evasion.ToString();//회피치
-        criticalChance.text = "치명타 확률 : " + playerStats.criticalChance.ToString();//치명타 확률
+        if (playerStats.Boots >= 0)//강화에 따른 회피 UP 부츠가 없으면 -1
+            evasion.text += " + (" + ItemStats.Boots[playerStats.Boots] + ")";
+
+        criticalChance.text = "치명타 확률 % : " + playerStats.criticalChance.ToString();//치명타 확률
+        if (playerStats.Gloves >= 0)//강화에 따른 치확 UP 장갑이 없으면 -1
+            criticalChance.text += " + (" + ItemStats.Gloves[playerStats.Gloves] + ") %";
+
         criticalDamagema.text = "치명타 데미지 : " + playerStats.criticalDamagema.ToString();//치명타 데미지
+        
         vampire.text = "체력 흡혈 % : " + playerStats.vampire.ToString();//체력 흡혈 %
+        if (playerStats.Cloak >= 0)//강화에 따른 체력흡혈 UP 망토가 없으면 -1
+            vampire.text += " + (" + ItemStats.Cloak[playerStats.Cloak] * 100 + ") %";
+
         goldBonus.text = "골드 보너스 : " + playerStats.goldBonus.ToString();//골드 보너스
+        
         experienceBonus.text = "경험치 보너스 : " + playerStats.experienceBonus.ToString();//경험치 보너스
+        
         experience.text = "보유 경험치 : " + playerStats.experience.ToString();//보유 경험치
+        
         gold.text = "보유 골드 : " + playerStats.gold.ToString();//보유 골드
+
+        ScreenGold.text = "Gold "+playerStats.gold.ToString(); //화면 상단 골드 표시
         
         Color color = PlayerImage.color;
         color.a = 1.0f;
